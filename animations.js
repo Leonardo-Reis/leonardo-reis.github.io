25092526b079c5513eea45cffd508bab8ec0961e
Leonardo Reis <leonardocarlosreis@gmail.com>
2021-12-27 09:02:33 -0300

    Mudanças para telas maiores

--- animations.js
+++ animations.js
@@ -1,6 +1,9 @@
 const foto = document.querySelector(".foto-container")
 const textos_bem = document.querySelectorAll(".texto-bem")
 const textos_quem = document.querySelectorAll(".texto-quem")
+const icons = document.querySelectorAll(".skill-option")
+const skillTitle = document.querySelector(".skills-title")
+const linhas = document.querySelectorAll(".linha")
 
 const elementos = [foto]
 textos_bem.forEach((texto) => {
@@ -26,5 +29,37 @@ const animaTitle = () => {
     })
 }
 
+const animaSkills = () => {
+    icons.forEach((icon, n) => {
+        setTimeout(() => {
+            icon.style.animationPlayState = "running"
+        }, 250*n)
+    })
+
+    linhas.forEach((linha) => {
+        linha.style.visibility = "visible"
+        linha.style.animationPlayState = "running"
+    })
+}
+
 animaTitle()
 animaQuem()
+
+setTimeout(() => {
+    const windowTop = window.pageYOffset + window.innerHeight*3/4
+    const skillsTop = skillTitle.offsetTop
+
+    if (windowTop > skillsTop) {
+        animaSkills()
+    }
+}, 2000);
+
+window.addEventListener('scroll', () => {
+    const windowTop = window.pageYOffset + window.innerHeight*3/4
+    const skillsTop = skillTitle.offsetTop
+
+    if (windowTop > skillsTop) {
+        animaSkills()
+    }
+    
+})
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const icons = document.querySelectorAll(".skill-option")
 const skillTitle = document.querySelector(".skills-title")
 const linhas = document.querySelectorAll(".linha")
 
+setTimeout(() => {
+    const windowTop = window.pageYOffset + window.innerHeight*3/4
+    const skillsTop = skillTitle.offsetTop
+
+    if (windowTop > skillsTop) {
+        animaSkills()
+    }
+}, 2000);
+
 window.addEventListener('scroll', () => {
     const windowTop = window.pageYOffset + window.innerHeight*3/4
     const skillsTop = skillTitle.offsetTop
@@ -13,6 +22,7 @@ window.addEventListener('scroll', () => {
 })
 
 const animaSkills = () => {
+    console.log('a')
     icons.forEach((icon, n) => {
         setTimeout(() => {
             icon.style.animationPlayState = "running"
