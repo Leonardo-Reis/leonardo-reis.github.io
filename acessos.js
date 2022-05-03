7f58cc6d7f9a359d1cadaa3bd1630b311d4cbe71
Leonardo Reis <leonardocarlosreis@gmail.com>
2022-05-02 21:10:47 -0300

    salva uuid para proximo acesso caso saia antes do delay

--- acessos.js
+++ acessos.js
@@ -3,23 +3,20 @@ const storage = localStorage.getItem('id')
 if (storage === null) {
     console.log('nunca tinha entrado')
 
-    uuidReq = fetch('https://www.api.leonardo-reis.com/return-id')
-    .then(response => {
-        return response.json()
-    }).then(jsonBody => {
-        localStorage.setItem('id', jsonBody.uuid)
+    const uuid = crypto.randomUUID()
 
-        const data = {
-            "uuid": jsonBody.uuid
-        }
-    
-        const unicoReq = fetch('https://www.api.leonardo-reis.com/add-acesso-unico', header= {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
+    localStorage.setItem('id', uuid)
+
+    const data = {
+        "uuid": uuid
+    }
+
+    fetch('https://www.api.leonardo-reis.com/add-acesso-unico', header= {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
     })
 } else {
     console.log('já tinha entrado')
